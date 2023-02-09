2c3b14ce2fe795e5f2b98cffa69793945466614d
Jason Brennan <i.jasonbrennan@gmail.com>
2023-02-09 10:15:54 -0500

    Initial Commit

---
+++ Smol/SmolApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmolApp.swift
+//  Smol
+//
+//  Created by Jason Brennan on 2/9/23.
+//
+
+import SwiftUI
+
+@main
+struct SmolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
